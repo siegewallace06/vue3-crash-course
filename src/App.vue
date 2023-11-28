000d6acd455c1af0503f0de53d73b1067a3ff810
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2023-11-28 15:03:57 +0700

    Add TodoHeader component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue'
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
